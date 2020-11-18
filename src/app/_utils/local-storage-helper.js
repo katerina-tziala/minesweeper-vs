a979e4c9ab162a4b9dd2d227b5cfa7c3a08cd8cb
katerina-tziala <katerina.tziala@gmail.com>
2020-11-19 00:07:31 +0100

    utils structure

--- src/app/_utils/local-storage-helper.js
+++ src/app/_utils/local-storage-helper.js
@@ -2,22 +2,22 @@
 
 export class LocalStorageHelper {
 
-	static save(keyName, value) {
-		localStorage.setItem(keyName, JSON.stringify(value));
-	}
+  static save(keyName, value) {
+    localStorage.setItem(keyName, JSON.stringify(value));
+  }
 
-	static retrieve(keyName) {
-		const item = localStorage.getItem(keyName);
-		return item ? JSON.parse(item) : undefined;
-	}
+  static retrieve(keyName) {
+    const item = localStorage.getItem(keyName);
+    return item ? JSON.parse(item) : undefined;
+  }
 
-	static remove(keyName) {
-		return localStorage.removeItem(keyName);
-	}
+  static remove(keyName) {
+    return localStorage.removeItem(keyName);
+  }
 
-	static clear() {
-		return localStorage.clear();
-	}
+  static clear() {
+    return localStorage.clear();
+  }
 
 }
 
--- src/app/_utils/utils.js
+++ src/app/_utils/utils.js
@@ -3,30 +3,30 @@
 import { TYPOGRAPHY } from "~/_constants/typography.constants.js";
 
 export const clone = (itemToClone) => {
-	return JSON.parse(JSON.stringify(itemToClone));
+  return JSON.parse(JSON.stringify(itemToClone));
 };
 
 export const sortNumbersArrayAsc = (arrayToSort) => {
-	return [...arrayToSort].sort((itemA, itemB) => { return itemA - itemB; });
+  return [...arrayToSort].sort((itemA, itemB) => { return itemA - itemB; });
 };
 
 export const getRandomValueFromArray = (arrayToChooseFrom) => {
-	return arrayToChooseFrom[Math.floor(Math.random() * arrayToChooseFrom.length)];
+  return arrayToChooseFrom[Math.floor(Math.random() * arrayToChooseFrom.length)];
 };
 
 export const preventInteraction = (event) => {
-	event.preventDefault();
-	event.stopPropagation();
+  event.preventDefault();
+  event.stopPropagation();
 };
 
 export const replaceStringParameter = (stringToUpdate, value, replacePart = TYPOGRAPHY.tripleHashtag) => {
-	return stringToUpdate.replace(replacePart, value);
+  return stringToUpdate.replace(replacePart, value);
 };
 
 export const extractIdValue = (elementId) => {
-	return elementId.split(TYPOGRAPHY.doubleUnderscore).pop();
+  return elementId.split(TYPOGRAPHY.doubleUnderscore).pop();
 };
 
 export const roundUpToNextDecade = (number) => {
-    return Math.ceil(parseInt(number) / 10) * 10;
+  return Math.ceil(parseInt(number) / 10) * 10;
 };
