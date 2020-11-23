699d41c2de8354107a2e9238f6298455ffe4a3cd
katerina-tziala <katerina.tziala@gmail.com>
2020-11-23 01:47:47 +0100

    Update game-page.js

--- src/app/pages/game-page/game-page.js
+++ src/app/pages/game-page/game-page.js
@@ -9,91 +9,72 @@ import { LocalStorageHelper } from "../../_utils/local-storage-helper";
 import { Page } from "../page";
 import { DOM_ELEMENT_ID, DOM_ELEMENT_CLASS, FORM_PARAMS } from "./game-page.constants";
 
-import { FormUsername } from "../../components/form/form-username/form-username";
-import { StateLoader } from "../../components/loaders/state-loader/state-loader";
-
 import { NOTIFICATION_MESSAGE } from "../../components/toast-notification/toast-notification.constants";
-
-// import { GameWizardOriginal, GameWizardVSBot, GameWizardVSFriend, GameWizardVSOnline } from "~/game/game-wizard/@game-wizard.module";
-
 
 import { User } from "~/_models/user";
 
 
 import { GameType } from "Game";
 
-
 export class GamePage extends Page {
-  #gameType;
-
-
-
-
+  #_gameType;
+  #_game;
 
   constructor(gameType, navigateToHome) {
     super();
-    this.gameType = gameType;
+    this.#gameType = gameType;
     this.navigateToHome = navigateToHome;
     this.game = undefined;
     self.settingsController.gameSettingsHidden = false;
     this.init();
   }
 
-  loadWizardClass(wizardPath) {
-    return import(`~/game/game-wizard/${wizardPath}`);
+  set #gameType(type) {
+    this.#_gameType = type;
   }
 
-  loadOriginalWizard() {
-    return this.loadWizardClass("game-wizard-original").then(({ GameWizardOriginal }) => {
-      return new GameWizardOriginal(this.navigateToHome, this.onPlayGame.bind(this));
+  get #gameType() {
+    return this.#_gameType;
+  }
+
+  set game(game) {
+    this.#_game = game;
+  }
+
+  get game() {
+    return this.#_game;
+  }
+
+  #loadWizard(wizardName) {
+    return import(`~/game/game-wizard/@game-wizard.module`).then(module => {
+      return new module[wizardName](this.navigateToHome, this.onPlayGame.bind(this));
     });
   }
 
-  loadVSBotWizard() {
-    return this.loadWizardClass("game-wizard-vs/game-wizard-vs-bot").then(({ GameWizardVSBot }) => {
-      return new GameWizardVSBot(this.navigateToHome, this.onPlayGame.bind(this));
-    });
-  }
-
-  loadVSFriendWizard() {
-    return this.loadWizardClass("game-wizard-vs/game-wizard-vs-friend").then(({ GameWizardVSFriend }) => {
-      return new GameWizardVSFriend(this.navigateToHome, this.onPlayGame.bind(this));
-    });
-  }
-
-  loadWizard() {
-    switch (this.gameType) {
+  #initializeWizard() {
+    switch (this.#gameType) {
       case GameType.Original:
-        return this.loadOriginalWizard();
+        return this.#loadWizard("GameWizardOriginal");
       case GameType.Bot:
-        return this.loadVSBotWizard();
+        return this.#loadWizard("GameWizardVSBot");
       case GameType.Friend:
-        return this.loadVSFriendWizard();
+        return this.#loadWizard("GameWizardVSFriend");
     }
   }
 
-  set gameType(type) {
-    this.#gameType = type;
-  }
-
-  get gameType() {
-    return this.#gameType;
-  }
-
   renderPage(mainContainer) {
-    this.loadWizard().then(gameWizard => {
+    this.#initializeWizard().then(gameWizard => {
       this.gameWizard = gameWizard;
       mainContainer.append(this.gameWizard.generateWizard());
     });
   }
 
   onPlayGame(game) {
-    //super.onConnectionError(errorMessage);
-    console.log(game);
     this.displayLoader();
     this.getClearedMainContainer().then(mainContainer => {
       console.log(mainContainer);
       console.log("game ooon");
+      this.game = game;
       this.hideLoader();
     });
   }
