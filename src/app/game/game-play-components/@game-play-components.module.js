db638937ee2de6488f677bafd4012727dc6f1475
katerina-tziala <katerina.tziala@gmail.com>
2020-12-07 20:03:08 +0100

    add player card in game play components

--- src/app/game/game-play-components/@game-play-components.module.js
+++ src/app/game/game-play-components/@game-play-components.module.js
@@ -2,3 +2,4 @@ export { DashboardFaceIcon } from "./dashboard-face-icon/dashboard-face-icon";
 export { DigitalCounter } from "./digital-counter/digital-counter";
 export { GameTimer } from "./game-timer/game-timer";
 export { MineField } from "./mine-field/mine-field";
+export { GamePlayerCard } from "./game-player-card/game-player-card";
