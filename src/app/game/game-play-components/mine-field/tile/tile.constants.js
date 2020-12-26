c8c72b4df4ed02c821005c41263ae2b34679acb1
katerina-tziala <katerina.tziala@gmail.com>
2020-12-26 18:10:47 +0100

    styles structure

--- src/app/game/game-play-components/mine-field/tile/tile.constants.js
+++ src/app/game/game-play-components/mine-field/tile/tile.constants.js
@@ -2,24 +2,24 @@
 
 export const DOM_ELEMENT_ID = {
   cell: "tile-cell__",
-  button: "tile-cell-btn__",
+  button: "tile-button__",
 };
 
 export const DOM_ELEMENT_CLASS = {
   cell: "tile-cell",
   cellContent: "tile-cell__content--",
-  activeButton: "tile-cell-btn--active",
+  activeButton: "tile-button--active",
   mineReveled: "tile-cell--revealed-mine",
-  button: "tile-cell-btn",
-  buttonColor: "tile-cell-btn-color--",
-  buttonFlagged: "tile-cell-btn--flagged",
-  buttonWronglyFlagged: "tile-cell-btn--wrongly-flagged",
-  buttonMarked: "tile-cell-btn--marked",
-  buttonRevealed: "tile-cell-btn--revealed",
+  button: "tile-button",
+  buttonColor: "tile-button-color--",
+  buttonFlagged: "tile-button--flagged",
+  buttonWronglyFlagged: "tile-button--wrongly-flagged",
+  buttonMarked: "tile-button--marked",
+  buttonRevealed: "tile-button--revealed",
 };
 
 export const TILE_BTN = {
-  className: "tile-cell-btn",
+  className: "tile-button",
   attributes: {
     "aria-label": "choose mine field tile",
   },
