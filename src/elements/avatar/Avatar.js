edd1b5f5f3b77b2d7559207243fe32a8d3671f8e
katerina-tziala <katerina.tziala@gmail.com>
2021-04-15 23:27:55 +0200

    custom elements

---
+++ src/elements/avatar/Avatar.js
@@ -0,0 +1,11 @@
+import "./avatar.scss";
+
+class Avatar extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+}
+
+customElements.define("app-avatar", Avatar);
--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
 import "./index.scss";
-import { AppLoader } from "./app/components/loaders/app-loader/app-loader";
 
-const loadApp = () => import("./app/app");
-const loadNotifications = () =>
-  import("./app/components/toast-notification/toast-notification");
+import "./elements/avatar/Avatar";
+// import { AppLoader } from "./app/components/loaders/app-loader/app-loader";
+
+// const loadApp = () => import("./app/app");
+// const loadNotifications = () =>
+//   import("./app/components/toast-notification/toast-notification");
 
 document.addEventListener("DOMContentLoaded", () => {
-  self.appLoader = AppLoader;
+  // self.appLoader = AppLoader;
+  console.log("loaded");
+  // loadNotifications().then(({ ToastNotifications }) => {
+  //   self.toastNotifications = new ToastNotifications();
 
-  loadNotifications().then(({ ToastNotifications }) => {
-    self.toastNotifications = new ToastNotifications();
+  //   loadApp().then(({ App }) => {
+  //     new App();
+  //   });
+  // });
+  const main = document.getElementById("main");
 
-    loadApp().then(({ App }) => {
-      new App();
-    });
-  });
+  // const avatar = document.createElement("app-avatar");
+  // //avatar.classList.add("test");
+  // avatar.className = "test";
+
+
+
+  // main.append(avatar);
+
+  // const avatars = document.getElementsByTagName("app-avatar");
+  // console.log(avatars);
+  // setTimeout(() => {
+  //   avatars[0].setAttribute("type", "bot");
+  //   avatars[0].className = "sdf";
+
+    
+  //   console.log("ddd");
+  // }, 2000);
 });
