0241f784431ee69d2a4637a4f3030782d47c78d8
katerina-tziala <katerina.tziala@gmail.com>
2021-04-22 00:34:30 +0200

    input element label animation

--- src/app/ui-elements/text-input/TextInput.js
+++ src/app/ui-elements/text-input/TextInput.js
@@ -1,20 +1,57 @@
 import './text-input.scss';
 import { ElementHandler } from '../../element-handler';
+import { DOM_ELEMENT_CLASS } from './text-input.constants';
 
 const template = `<label class='text-input__label'>username</label>
-<input class='text-input__input' type='text' placeholder='username' name='username'
+<input class='text-input__input' type='text' name='username'
 value='kate' autocomplete='off'/>`;
 
 class TextInput extends HTMLElement {
-
+  #label;
+  #input;
+  #labelListener;
+ 
   constructor() {
     super();
   }
 
   connectedCallback() {
     this.innerHTML = template;
+    this.#label = this.querySelector(`.${DOM_ELEMENT_CLASS.label}`);
+    this.#input = this.querySelector(`.${DOM_ELEMENT_CLASS.input}`);
+
+    this.#addLabelListener();
+
+    this.#input.addEventListener('focus', this.#focus.bind(this));
+    this.#input.addEventListener('focusout', this.#focusout.bind(this));
   }
 
+  #onLabelClick(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.#input.focus();
+  }
+
+  #focus() {
+    this.#label.classList.add(DOM_ELEMENT_CLASS.focusedLabel);
+    this.#removeLabelListener();
+  }
+
+  #focusout() {
+    this.#label.classList.remove(DOM_ELEMENT_CLASS.focusedLabel);
+    this.#addLabelListener();
+  }
+
+  #addLabelListener() {
+    this.#labelListener = this.#onLabelClick.bind(this);
+    this.#label.addEventListener('click', this.#labelListener);
+  }
+
+  #removeLabelListener() {
+    if (this.#labelListener) {
+      this.#label.removeEventListener('click', this.#labelListener);
+    }
+  }
 }
 
 customElements.define('app-text-input', TextInput);
---
+++ src/app/ui-elements/text-input/text-input.constants.js
@@ -0,0 +1,5 @@
+export const DOM_ELEMENT_CLASS = {
+  input: "text-input__input",
+  label: "text-input__label",
+  focusedLabel: "text-input__label--input-focused"
+};
