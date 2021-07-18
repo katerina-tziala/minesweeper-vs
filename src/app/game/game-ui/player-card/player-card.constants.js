0481dffd98f5ce4f43e82d4f978e48c632b37c8f
katerina-tziala <katerina.tziala@gmail.com>
2021-07-19 00:38:26 +0300

    player card

--- src/app/game/game-ui/player-card/player-card.constants.js
+++ src/app/game/game-ui/player-card/player-card.constants.js
@@ -1,6 +1,7 @@
 'use strict';
 export const DOM_ELEMENT_CLASS = {
-    playerContainer: 'player-card__player-container'
+    playerContainer: 'player-card__player-container',
+    indicators: 'player-card__indicators'
 };
 
 export const ATTRIBUTES = {
@@ -10,9 +11,9 @@ export const ATTRIBUTES = {
 export const TEMPLATE = `
 <div class='${DOM_ELEMENT_CLASS.playerContainer}'>
     <app-player-turns direction='rtl' turns=10></app-player-turns>
-    <app-avatar></app-avatar>
+    <app-avatar type='user'></app-avatar>
 </div>
-<div>
+<div class='${DOM_ELEMENT_CLASS.indicators}'>
     <app-placed-flags flag-type="checkered" color-type="type-4" value="3"></app-placed-flags>
 </div>
 `;
